72016658ebb5dad45bf1bd519b4262a94564dd76
Alicia Wynona Tjahjadi <alicia@intranet.rosadelsvents.es>
2026-01-20 14:06:34 +0100

    Answer debug recipe

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,4 +1,8 @@
 // Predict and explain first...
+// Calling ${recipe} will not log the ingredients because recipe is an object
+// Instead, template literal will log toString() of the object
+// So, [object Object] will be logged into the console
+// Because JS is telling us that this is an object type object.
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
@@ -11,5 +15,7 @@ const recipe = {
 };
 
 console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+ingredients:`);
+for (const elements of recipe.ingredients) {
+  console.log(elements);
+}
