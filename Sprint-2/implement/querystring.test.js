2abffcac92905b37b86bd5117d62bd81b95a8f0e
Alicia Wynona Tjahjadi <alicia@intranet.rosadelsvents.es>
2026-01-23 16:54:22 +0100

    Answer implement query string

--- Sprint-2/implement/querystring.test.js
+++ Sprint-2/implement/querystring.test.js
@@ -5,8 +5,30 @@
 
 const parseQueryString = require("./querystring.js")
 
-test("parses querystring values containing =", () => {
+test("parses querystring values containing multiple =", () => {
   expect(parseQueryString("equation=x=y+1")).toEqual({
     "equation": "x=y+1",
   });
 });
+
+test("parses querystring values without =", () => {
+  expect(parseQueryString("hello&hai")).toEqual({
+    "hello": "", "hai": ""
+  });
+})
+
+test("parses querystring values with no value", () => {
+  expect(parseQueryString("")).toEqual({});
+})
+
+test("parses querystring values with trailing & and multiple &", () => {
+  expect(parseQueryString("&hello=hola&&&hai=holi&")).toEqual({
+    "hello":"hola", "hai":"holi"
+  })
+})
+
+test("parses querystring values with same keys", () => {
+  expect(parseQueryString("name=Alicia&name=Valerie&name=Deedee")).toEqual({
+    "name": ["Alicia", "Valerie", "Deedee"]
+  })
+})
