da554a27627807bee842c9e836cbc7b578c2fddf
Alicia Wynona Tjahjadi <alicia@intranet.rosadelsvents.es>
2026-02-04 15:00:12 +0100

    Add auto-next and auto-previous functionality

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -9,6 +9,9 @@ const images = [
 const container = document.getElementById("carousel")
 const previousButton = document.getElementById("previous-btn");
 const nextButton = document.getElementById("next-btn");
+const autoPreviousButton = document.getElementById("auto-previous-btn");
+const autoNextButton = document.getElementById("auto-next-btn");
+const stopButton = document.getElementById("stop-btn");
 
 const createImages = (images) => {
     const img = document.createElement("img");
@@ -29,7 +32,10 @@ updateButtons();
 
 function updateButtons() {
     previousButton.disabled = current === 0;
+    autoPreviousButton.disabled = current === 0;
+
     nextButton.disabled = current === imageCards.length - 1;
+    autoNextButton.disabled = current === imageCards.length - 1;
 }
 
 function showCurrent() {
@@ -50,4 +56,44 @@ previousButton.addEventListener("click", () => {
         current--;
         showCurrent();
     }
+})
+
+let interval;
+
+autoNextButton.addEventListener("click", () => {
+    if (interval) return;
+
+    autoNextButton.disabled = true;
+    autoNextButton.classList.add("running");
+
+    interval = setInterval (() => {
+        if (current < (imageCards.length - 1)) {
+            current++;
+            showCurrent();
+        } else {
+            clearInterval(interval);
+            interval = null;
+            autoNextButton.classList.remove("running");
+            showCurrent();
+        }
+    }, 1000);
+})
+
+autoPreviousButton.addEventListener("click", () => {
+    if (interval) return;
+
+    autoPreviousButton.disabled = true;
+    autoPreviousButton.classList.add("running");
+
+    interval = setInterval (() => {
+        if (current > 0) {
+            current--;
+            showCurrent();
+        } else {
+            clearInterval(interval);
+            interval = null;
+            autoPreviousButton.classList.remove("running");
+            showCurrent();
+        }
+    }, 1000);
 })
