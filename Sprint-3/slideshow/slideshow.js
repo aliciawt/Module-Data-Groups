1e9d78f0c81b93b1f5dfd346752afa3d2cda9562
Alicia Wynona Tjahjadi <alicia@intranet.rosadelsvents.es>
2026-02-04 13:09:28 +0100

    Slideshow Level 1

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -1,8 +1,47 @@
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
+    {source: "./assets/cute-cat-a.png", alt: "a grey tabby kitten"},
+    {source: "./assets/cute-cat-b.jpg", alt: "a brown tabby kitten"},
+    {source: "./assets/cute-cat-c.jpg", alt: "an orange kitten"},
+    {source: "./assets/cute-cat-d.png", alt: "a shoulder cat"}
 ];
 
 
-// Write your code here
+const container = document.getElementById("carousel")
+
+const createImages = (images) => {
+    const img = document.createElement("img");
+    img.src = images.source;
+    img.alt = images.alt;
+    img.style.maxWidth = "500px";
+    img.style.maxHeight = "400px";
+    img.style.objectFit = "contain";
+    return img;
+}
+
+const imageCards = images.map(createImages);
+
+let current = 0;
+
+container.append(imageCards[current]);
+
+const previousButton = document.getElementById("previous-btn");
+const nextButton = document.getElementById("next-btn");
+
+function showCurrent() {
+    container.innerHTML = "";
+    container.append(imageCards[current]);
+}
+
+nextButton.addEventListener("click", () => {
+    if (current < (imageCards.length - 1)) {
+        current = (current + 1);
+        showCurrent();
+    }
+})
+
+previousButton.addEventListener("click", () => {
+    if (current > 0) {
+        current = (current - 1);
+        showCurrent();
+    }
+})
